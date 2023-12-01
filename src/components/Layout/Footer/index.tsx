d4a5f27a5f146075f709010a36f7f228b8994765
phantrunghieu91 <phantrunghieu1291@gmail.com>
2023-12-01 14:18:43 +0700

    fixed header and footer height

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -4,7 +4,7 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = ({ className }) => {
   return (
-    <footer id='footer' className={`mx-auto container flex flex-nowrap pt-4 mt-8 justify-center border-t border-sky-100 ${className !== undefined ? className : ''}`}>
+    <footer id='footer' className={`mx-auto container flex flex-nowrap py-4 mt-8 justify-center border-t border-sky-100 ${className !== undefined ? className : ''}`}>
       Copyright 2023 - All Right Reserved
     </footer>
   );
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -10,10 +10,10 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ className, children }) => {
   const borderColor = 'border-slate-600';
   return (
-    <div id='page-wrapper' className={`h-screen dark:bg-black dark:text-white bg-blue-500 text-slate-900 overflow-hidden`}>
-      <Header className={`h-[10%] ${borderColor}`} />
-      <main className={`overflow-auto container mx-auto mt-8 h-[70%] ${borderColor} ${className !== undefined ? className : ''}`}>{children}</main>
-      <Footer className={`h-[20%] ${borderColor}`} />
+    <div id='page-wrapper' className={`h-screen dark:bg-black dark:text-white bg-blue-500 text-slate-900 overflow-hidden flex flex-col`}>
+      <Header className={`h-[100px] ${borderColor}`} />
+      <main className={`overflow-auto container mx-auto mt-8 grow ${borderColor} ${className !== undefined ? className : ''}`}>{children}</main>
+      <Footer className={`h-[auto] ${borderColor}`} />
     </div>
   );
 };
