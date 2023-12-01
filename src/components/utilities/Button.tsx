5da77db874dce4819931de4cd87529e22f3bc3fb
phantrunghieu91 <phantrunghieu1291@gmail.com>
2023-12-01 17:14:43 +0700

    changed btnBgColor, add handleClick function

--- src/components/utilities/Button.tsx
+++ src/components/utilities/Button.tsx
@@ -1,13 +1,17 @@
+import { MouseEventHandler } from 'react';
+
 interface ButtonProps {
   type?: 'button' | 'submit' | 'reset';
   className?: string;
   children: React.ReactNode;
+  bgColor?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button: React.FC<ButtonProps> = ({ type = 'button', className, children }) => {
-  const btnBackgroundClr = type === 'submit' ? 'bg-green-600 hover:bg-green-500' : type === 'reset' ? 'bg-red-600 hover:bg-red-500' : 'bg-blue-600 hover:bg-blue-500';
+const Button: React.FC<ButtonProps> = ({ type = 'button', className, children, bgColor, handleClick }) => {
+  const btnBackgroundClr = bgColor !== undefined ? bgColor : type === 'submit' ? 'bg-green-600 hover:bg-green-500' : type === 'reset' ? 'bg-red-600 hover:bg-red-500' : 'bg-blue-600 hover:bg-blue-500';
   return (
-    <button type={type} className={`${className ? className : ''} ${btnBackgroundClr} px-4 py-2 uppercase font-semibold transition-colors duration-500 `}>
+    <button type={type} className={`${className ? className : ''} ${btnBackgroundClr} px-4 py-2 uppercase font-semibold transition-colors duration-500 `} onClick={handleClick}>
       {children}
     </button>
   );
