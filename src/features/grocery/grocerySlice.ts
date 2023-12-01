af24cb3d10c8d2185e6721b3eead7f6bc503203a
phantrunghieu91 <phantrunghieu1291@gmail.com>
2023-12-01 16:09:56 +0700

    export actions, selector and add type for payload

--- src/features/grocery/grocerySlice.ts
+++ src/features/grocery/grocerySlice.ts
@@ -1,32 +1,44 @@
-import { Action, createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { RootState } from '../../store';
 
-type GroceryItem = {
+export type GroceryItem = {
   id: number;
   name: string;
   quantity: number;
 };
 
-const initialState = {
-  groceryList: new Array<GroceryItem>(),
+export type GroceryList = GroceryItem[];
+
+export interface GroceryState {
+  count: number;
+  groceryList: GroceryList;
+}
+
+const initialState: GroceryState = {
+  count: 0,
+  groceryList: [],
 };
 
 const grocerySlice = createSlice({
   name: 'grocery',
   initialState,
   reducers: {
-    add: (state, { payload }) => {
+    add: (state, { payload }: PayloadAction<GroceryItem>) => {
+      state.count++;
       state.groceryList = [...state.groceryList, payload];
     },
-    update: (state, { payload }) => {
+    update: (state, { payload }: PayloadAction<GroceryItem>) => {
       state.groceryList = state.groceryList.map(item => {
         if (item.id === payload.id) item = payload;
         return item;
       });
     },
-    remove: (state, { payload }) => {
+    remove: (state, { payload }: PayloadAction<number>) => {
       state.groceryList.filter(item => item.id !== payload);
     },
   },
 });
 
+export const { add, update, remove } = grocerySlice.actions;
+export const selectCount: (state: RootState) => number = state => state.grocery.count;
 export default grocerySlice.reducer;
